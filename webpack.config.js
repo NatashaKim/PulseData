65ac1b9ce088ee2c76ac481d296135429ca49ed4
Natasha <natasha.kim333@gmail.com>
2022-06-19 13:25:46 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dev_build'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
